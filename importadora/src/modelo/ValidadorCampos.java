ca68cd0a497f134dfdd3a7c4dbb2a2597ef5ab22
OsmarSJ10 <osmarsj2007@gmail.com>
2017-04-16 22:16:01 -0400

    Implementando nuevos validadores

--- importadora/src/modelo/ValidadorCampos.java
+++ importadora/src/modelo/ValidadorCampos.java
@@ -7,6 +7,8 @@ package modelo;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 /**
  *
@@ -34,4 +36,73 @@ public class ValidadorCampos {
 //            System.out.println("El email ingresado es inválido.");
         }
     }
+    public String quitarEspacios(String w){
+        String se=" ";
+        int contador=0;
+        String sTexto=w;
+        
+        for (int x=0; x < sTexto.length(); x++) {    
+            if (sTexto.charAt(x)!=' '){
+                se+=sTexto.charAt(x);
+                contador=0;
+            }else{
+                contador++;
+                if(contador<2){
+                se+=sTexto.charAt(x);
+                }
+            }
+
+        }
+        return se;
+     }
+    public boolean validarDireccion(JTextField n){
+        JTextField x=new JTextField();
+        String dir=n.getText();
+        String caracteres=";'[]{}:,<>'=_-!@$%^&*()/*-+";
+        for(int i=0;i<dir.length();i++){
+           for(int j=0;j<caracteres.length();j++){
+                if(dir.charAt(i)==caracteres.charAt(j)){            
+                        x.setText("");
+                        JOptionPane.showMessageDialog(null, "El nombre de la dirección es invalido");
+                        return true;
+                    
+                }
+           }
+           
+        }
+        return false;
+    }
+    public boolean validarNombre(JTextField n){
+        JTextField x=new JTextField();
+        String nom=n.getText();
+        String caracteres="0123456789.:,<>{}[]|'=_-!@#$%^&*()/*-+";
+        for(int i=0;i<nom.length();i++){
+           for(int j=0;j<caracteres.length();j++){
+                if(nom.charAt(i)==caracteres.charAt(j)){            
+                        x.setText("");
+                        JOptionPane.showMessageDialog(null, "Nombre no es valido");
+                        return true;
+                    
+                }
+           }
+           
+        }
+        return false;
+    }
+    public boolean validarCorreo2(JTextField correo){
+        String email=correo.getText();
+        Pattern pattern = Pattern
+                .compile("^[_a-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+
+        Matcher mather = pattern.matcher(email);
+ 
+        if (mather.find() == true) {
+            return  true;
+        } else { 
+            JOptionPane.showMessageDialog(null, "El correo es invalido");
+            return  false;
+        }
+    }
+
 }
